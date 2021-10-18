cc5e05ee45f787fc0682e11cfef5de5a4ab1548c
Ben Liu <bliu033@gmail.com>
2021-10-19 10:17:09 +1300

    issue

--- helloCodeQl/helloCodeQl/Program.cs
+++ helloCodeQl/helloCodeQl/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 
 namespace helloCodeQl
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        //uri from para should picked up 
+        public async Task<HttpResponseMessage> GetRespondAsync(Uri uri)
+        {
+            client.BaseAddress = uri;
+            var res = await client.GetAsync(uri);
+            return res;
         }
     }
 }
